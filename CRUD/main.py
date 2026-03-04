7eb127d21856e4b7277523c9940c74cdd7a7e171
Muhammad Umer Adnan <omeradnannovember2005@gmail.com>
2026-03-04 21:13:54 +0500

    update request
    added patch request for updating content

--- CRUD/main.py
+++ CRUD/main.py
@@ -20,6 +20,9 @@ class Product(BaseModel):
   id: int
   name: str
   price: float
+class ProductUpdateModel(BaseModel):
+  name: str
+  price: float
 @app.get('/')
 async def intro():
   return {"message":"A FastApi CRUD for products"}
@@ -37,10 +40,15 @@ async def get_product(product_id:int) -> dict:
     if product['id'] == product_id:
        return product
   raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Product Not Found OF ID:{product_id}")
+@app.patch('/products/{product_id}')
+async def update_product(product_id:int,product_update_data:ProductUpdateModel)-> dict:
+  for product in products:
+    if product['id'] == product_id:
+      product['name'] = product_update_data.name
+      product['price'] = product_update_data.price
+      return product
+  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Product Not Found OF ID:{product_id}")
 @app.get('/products/{product_id}')
-async def update_product(book_id:int):
-  pass
-@app.get('/products/{product_id}')
-async def delete_product(book_id:int):
+async def delete_product(product_id:int):
   pass
 
